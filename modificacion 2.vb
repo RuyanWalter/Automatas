076e1d2d3124850a498940bd9a93705b13a46b98
RuyanWalter <Sirin23ruyan@gmail.com>
2023-10-11 15:47:31 -0600

    Este es el codigo a evaluar VB

---
+++ modificacion 2.vb
@@ -0,0 +1,39 @@
+Private Sub CmdCalcular_Click()
+    TxtResultado.Text = Val(TxtNumero1.Text) + Val(TxtNumero2.Text)
+End Sub
+
+Private Sub CmdCerrar_Click()
+    Unload Me
+    End
+End Sub
+
+Private Sub CmdLimpiar_Click()
+    TxtNumero1.Text = ""
+    TxtNumero2.Text = ""
+    TxtResultado.Text = ""
+    TxtNumero1.SetFocus
+End Sub  d d
+
+Or if =  "hola mundo" <
+ 4 45E2 4.2 
+< Dim != if ka ll 15.20 10E-10
+.50 .30 0.3 10E+10 dd 23  qqq
+w  34r
+3..4
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
